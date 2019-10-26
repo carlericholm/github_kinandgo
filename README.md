d0c395fb196e1a774eaf59922afa5e05653f2145
carl-eric <carlericholm@gmail.com>
2019-10-26 12:14:10 +0200

    version finale 1 kin&go

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# github_kinandgo
-physiotherapy website
+# github_KINANDGO
+KIN&amp;GO website
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+app.get('/kinegenerale', (req, res) => {
+    res.render("kiGenerale");
+})
+
+app.get('/kinesportive', (req, res) => {
+    res.render("kiSportive");
+})
+
+app.get('/suivi', (req, res) => {
+    res.render("suivi");
+})
+
+app.get('/contact', (req, res) => {
+    res.render("contact");
+})
+
+app.use((req, res, next) => {
+    res.redirect('/');
+})
+
+app.listen(PORT, () => {
+    console.log("Server listenning on port 8080");
+})
