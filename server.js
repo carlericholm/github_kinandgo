13f886dd817a3a729064b25cc7cac002ce17ea8f
carl-eric <carlericholm@gmail.com>
2019-10-26 12:48:04 +0200

    version finale 1 kin&go

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const app = express();
+
+// const PORT = 80;
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+
+
+
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
-const PORT = 80;
+
 
 app.set('view engine', 'ejs');
 
@@ -33,6 +55,23 @@ app.get('/contact', (req, res) => {
 //     res.redirect('/');
 // })
 
-app.listen(PORT, () => {
-    console.log("Server listenning on port 80");
-})
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+
+
+
+
+
+// app.listen(PORT, () => {
+//     console.log("Server listenning on port 80");
+// })
